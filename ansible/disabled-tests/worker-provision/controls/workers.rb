d945fb402305818b00d73bfa9d0747b75b372712
Samuel Michael Squire <sam@samsquire.com>
2020-06-19 12:42:21 +0100

    Sam disabled tests

---
+++ ansible/disabled-tests/worker-provision/controls/workers.rb
@@ -0,0 +1,12 @@
+describe command('terraform') do
+  it { should exist }
+end
+describe command('packer') do
+  it { should exist }
+end
+describe command('inspec') do
+  it { should exist }
+end
+describe command('gradle') do
+  it { should exist }
+end
